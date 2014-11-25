6c37d7d931da7b18ad5efd61c3fa60b918375d03
zacatac <field.zackery@gmail.com>
2014-11-24 20:16:50 -0800

    adding buildpack files and adjusted gruntfile and package.json

--- client/Gruntfile.js
+++ client/Gruntfile.js
@@ -446,6 +446,8 @@ module.exports = function (grunt) {
     'karma'
   ]);
 
+  grunt.registerTask('heroku:production', 'build');
+  
   grunt.registerTask('build', [
     'clean:dist',
     'wiredep',
