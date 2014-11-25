b1600e6994a7958f46dc6087becca6139f4a3c05
zacatac <field.zackery@gmail.com>
2014-11-24 23:00:16 -0800

    spelling error

--- config/application.rb
+++ config/application.rb
@@ -27,7 +27,7 @@ module Goodmatches
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    config.assets.path << Rails.root.join("client/app/scripts")
+    config.assets.paths << Rails.root.join("client/app/scripts")
     
     config.generators do |g|
       g.test_framework :rspec,
